6722be9299ff8f70219c95e9ac681cd3c31dd9f4
Algalyq <algalyq@mail.ru>
2022-10-24 21:48:51 +0600

    kerek emes

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react'
-
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import useToken from './auth/useToken';
+import Login from './auth/Login';
 function App() {
-      return (
-          <h1>Test</h1>
-      );
   
-}
+  const { token, setToken } = useToken();
 
+ 
+
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login">
+            <Login setToken={setToken}/>
+          </Route>
+          <Route path='/'>
+          <main/>
+          </Route>
+
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
 export default App;
---
+++ front-app/src/components/Main.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+
+
+
+export default function Main() {
+
+    return(
+        <div className="login-wrapper">
+          <h1>test main</h1>
+        </div>
+      )
+    
+
+  }
+  
