d5588dc466f6d2d57b397ea66128893702182a30
Algalyq <algalyq@mail.ru>
2022-10-24 18:16:34 +0600

    New paper

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -1,66 +1,10 @@
-import React, { Component } from 'react';
-import {Route, Switch, BrowserRouter, Redirect} from 'react-router-dom';
+import React from 'react'
 
-import { Provider, connect } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-
-import {auth} from "./actions/auth";
-import Login from "./components/Login";
-
-let store = createStore( applyMiddleware(thunk));
-
-class RootContainerComponent extends Component {
-
-    componentDidMount() {
-        this.props.loadUser();
-    }
-
-    PrivateRoute = ({component: ChildComponent, ...rest}) => {
-        return <Route {...rest} render={props => {
-            if (this.props.auth.isLoading) {
-                return <em>Loading...</em>;
-            } else if (!this.props.auth.isAuthenticated) {
-                return <Redirect to="/login" />;
-            } else {
-                return <ChildComponent {...props} />
-            }
-        }} />
-    }
-
-    render() {
-        return (
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/login" component={Login} />
-                </Switch>
-            </BrowserRouter>
-        );
-    }
+function App() {
+      return (
+          <h1>Test</h1>
+      );
+  
 }
 
-const mapStateToProps = state => {
-    return {
-        auth: state.auth,
-    }
-}
-
-const mapDispatchToProps = dispatch => {
-    return {
-        loadUser: () => {
-            return dispatch(auth.loadUser());
-        }
-    }
-}
-
-let RootContainer = connect(mapStateToProps, mapDispatchToProps)(RootContainerComponent);
-
-export default class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <RootContainer />
-            </Provider>
-        )
-    }
-}
+export default App;
